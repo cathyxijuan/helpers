c259ba0562a398bd102f7926fac749ba34df5730
cathyxijuan <cathyxijuan@gmail.com>
2024-08-06 05:12:24 -0400

    add help page

--- R/cfi_fit.R
+++ R/cfi_fit.R
@@ -10,5 +10,4 @@ cfi_fit <- function(x){
   }
 }
 
-test
-test
+
--- R/rmsea_fit.R
+++ R/rmsea_fit.R
@@ -1,4 +1,11 @@
-
+#' RMSEA FIT
+#' This function allows you to check the model fit through RMSEA.
+#' This function allows you to express your love of cats.
+#' @param x a numeric value indicating the RMSEA value
+#' @keywords rmsea
+#' @export
+#' @examples
+#' rmsea_fit(0.04)
 rmsea_fit <- function(x){
   if (x < 0.08){
     if (x < 0.05){
