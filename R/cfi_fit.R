9a2aac7da0a18d62d76c4f06567471e69dfca08b
Marclianjie <lianjieshi02@gmail.com>
2024-08-08 12:09:12 -0700

    add roxygen skeleton

--- R/cfi_fit.R
+++ R/cfi_fit.R
@@ -1,3 +1,15 @@
+
+#' Title
+#'
+#' @param x cfi value
+#'
+#' @return a string describing the cfi value
+#' @export
+#'
+#' @examples
+#' cfi_fit(0.91)
+#' cfi_fit(0.96)
+#' cfi_fit(0.88)
 cfi_fit <- function(x){
   if (x > 0.90){
     if (x > 0.95){
